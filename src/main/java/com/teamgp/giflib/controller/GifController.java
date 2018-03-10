8a6830ac670aa0d3ad21e12b333d31cab4189aa9
gpborla+github@gmail.com <gpborla+github@gmail.com>
2018-03-10 19:20:29 +0800

    Initial commit

---
+++ src/main/java/com/teamgp/giflib/controller/GifController.java
@@ -0,0 +1,20 @@
+package com.teamgp.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String listGifs(){
+        return "List of all the GIFs";
+    }
+
+    @RequestMapping("gif")
+    @ResponseBody
+    public String showGif(){
+        return "Displaying GIF!";
+    }
+}
