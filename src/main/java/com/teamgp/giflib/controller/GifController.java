2eed86350bff022f3e5161fe7119bbd9ac83ab71
gpborla+github@gmail.com <gpborla+github@gmail.com>
2018-03-11 19:14:46 +0800

    mapped /gif to show gif from Gif model via ModelMap using ThymeLeaf

--- src/main/java/com/teamgp/giflib/controller/GifController.java
+++ src/main/java/com/teamgp/giflib/controller/GifController.java
@@ -1,8 +1,12 @@
 package com.teamgp.giflib.controller;
 
+import com.teamgp.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -11,9 +15,16 @@ public class GifController {
         return "home";
     }
 
-    @RequestMapping("/gif")
+    @RequestMapping("/showgif")
     @ResponseBody
     public String showGif(){
         return "Displaying GIF!";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("cowboy-coder", LocalDate.of(2017,5,18), "paolo",     true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
