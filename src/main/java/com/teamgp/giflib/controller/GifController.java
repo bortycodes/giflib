5fab7a7a3aa843854ca5a7300528b5c6d291c70e
gpborla+github@gmail.com <gpborla+github@gmail.com>
2018-03-12 15:09:36 +0800

    used @pathvariables to display requested gif and its details.

--- src/main/java/com/teamgp/giflib/controller/GifController.java
+++ src/main/java/com/teamgp/giflib/controller/GifController.java
@@ -5,6 +5,7 @@ import com.teamgp.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,9 +27,9 @@ public class GifController {
         return "Displaying GIF!";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap){
-        Gif gif = gifRepository.findByName("android-explosion");
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
